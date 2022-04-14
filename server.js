3b59744f2977b8d4b030d589da7853ad2535ec3e
Sabotage <nettoolslive@gmail.com>
2022-04-14 15:16:01 -0300

    Desenv

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ const Sequelize = require('sequelize');
 
 
 //DESENVOLVIMENTO
-  let publicKeyML = 'TEST-9a65716f-a1fa-4a23-8152-eac77271bcae';
-  let accessToken = 'TEST-4788801943068672-032721-c9e6cbd022064bda1cf9c41260deaf94-1096864621';
-  const token = '5297559808:AAFwjOXIeBbsK9vY0KMMIn1fvaJWCC2ooZ4';
+//  let publicKeyML = 'TEST-9a65716f-a1fa-4a23-8152-eac77271bcae';
+//  let accessToken = 'TEST-4788801943068672-032721-c9e6cbd022064bda1cf9c41260deaf94-1096864621';
+//  const token = '5297559808:AAFwjOXIeBbsK9vY0KMMIn1fvaJWCC2ooZ4';
 
 
 //PRODUCAO
